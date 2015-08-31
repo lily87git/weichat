4e66fc05bb0655e1462e0a2f221e6a9929344ddd
zl <zl@163.com>
2015-08-31 09:08:28 +0800

    初始化项目

---
+++ weichat/weichat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weichat
+//
+//  Created by Apple on 15/8/31.
+//  Copyright (c) 2015年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
